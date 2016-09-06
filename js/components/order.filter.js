a9fb12e21bac8f6779a7852ee1ba8a65eaa01c0a
Cyclodex <cyclodex@nothing.ch>
2016-09-06 22:03:03 +0200

    Trying to merge the Visual changes into the master after getting the webpack branch. Does it work?
    Merged the code, resolved conflicts and pushed the new order filter into the separate structured file system.
    refs #15

---
+++ js/components/order.filter.js
@@ -0,0 +1,14 @@
+// Help to order a object in the other way (limitation of firebase)
+angular.module('cycloneApp').filter('orderObjectBy', function() {
+    return function(items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if(reverse) filtered.reverse();
+        return filtered;
+    };
+});
--- js/cyclone.js
+++ js/cyclone.js
@@ -7,6 +7,7 @@
 // Here we require all the code needed so webpack knows what to load.
 require('./cyclone.module.js');
 require('./components/focus.directive.js');
+require('./components/order.filter.js');
 require('./user/profile.controller.js');
 require('./user/connection.controller.js');
 require('./time/time.controller.js');
