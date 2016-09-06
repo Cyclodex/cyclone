17774ca0f3c25469940823581377ec695b1f3e45
Patrick Fiaux <pfiaux@vt.edu>
2016-09-06 10:54:52 +0200

    Setup browser sync for running with reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
+'use strict';
+/* jshint esversion: 6 */
+/* jshint node: true */
+
 /**
  * Cyclone webpack build.
+ * Build by running `npm run build`
+ * For development use `npm start`
  */
+const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
 
 module.exports = {
     entry: [
       "./less/cyclone.less",
-      "./js/cyclone.js"
+      "./js/cyclone.js",
     ],
     output: {
         path: __dirname + '/public/build/',
@@ -22,6 +31,14 @@ module.exports = {
     },
     // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
     plugins: [
-        new ExtractTextPlugin("cyclone.css")
+        new ExtractTextPlugin("cyclone.css"),
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['public'] }
+        })
+
     ]
 };
