dae1d77b243f9e75ac48a3e479926e6e83f4ae8c
Cyclodex <cyclodex@nothing.ch>
2017-03-02 00:59:11 +0100

    updating date

--- js/version/version.service.js
+++ js/version/version.service.js
@@ -5,7 +5,7 @@ angular.module('cycloneApp').factory('versionService', [function() {
     }
 
     VersionService.prototype.getVersion = function() {
-        return '0.44 | 28.02.2017';
+        return '0.44 | 02.03.2017';
     }
 
     VersionService.prototype.getChanglogUrl = function() {
