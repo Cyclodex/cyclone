4ab7cd917fca31a1ae0637b2f054d6b2e021ff26
Cyclodex <cyclodex@nothing.ch>
2017-06-10 14:47:12 +0200

    Removing config.js from git.

--- js/config/config.js
+++
@@ -1,7 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDsWGJoeAa_1puNxV5gt0WMrtN12JIEKTk",
-    authDomain: "cyclone-806dd.firebaseapp.com",
-    databaseURL: "https://cyclone-806dd.firebaseio.com"
-};
-firebase.initializeApp(config);
