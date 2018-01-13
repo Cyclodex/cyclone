45679e06639bea9708de358431ad8d58532fcbfc
Fabian Gander <gander@jumps.ch>
2018-01-13 16:53:34 +0100

    1.0.0-beta3

--- js/version/version.js
+++ js/version/version.js
@@ -1,2 +1,2 @@
-// This file was generated on Sa 13 Jan 2018 16:30:15 CET
-export const appVersion = '0.0.0';
+// This file was generated on Sa 13 Jan 2018 16:53:34 CET
+export const appVersion = '1.0.0-beta3';
