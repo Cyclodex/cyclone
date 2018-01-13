cc8acd0ecb534f4c844f35f714d8555cea0b1bf3
Fabian Gander <gander@jumps.ch>
2018-01-13 16:53:25 +0100

    git add

--- version.sh
+++ version.sh
@@ -2,4 +2,4 @@ version_file=js/version/version.js
 > $version_file
 echo "// This file was generated on $(date)
 export const appVersion = '$1';" >> $version_file
-#git add $version_file
+git add $version_file
