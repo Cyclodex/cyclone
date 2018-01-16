e3791cb34dd513f1924d3254b08708f73ee22db5
Fabian Gander <gander@jumps.ch>
2018-01-16 16:35:40 +0100

    Date Simulation changes.

--- js/components/dateSimulation/dateSimulation.controller.js
+++ js/components/dateSimulation/dateSimulation.controller.js
@@ -1,4 +1,4 @@
-function DateSimulationController(moment) {
+function DateSimulationController(moment, $state, $timeout) {
     var ctrl = this;
     ctrl.dateSimulation = false;
 
@@ -10,15 +10,17 @@ function DateSimulationController(moment) {
         // Date simulation (for testing day jumps)
         if (ctrl.dateSimulation){
             // console.log("Simulation: ON");
+            // Re-defines what today is for moment()
             today = moment(ctrl.dateSimulation);
             moment.now = function () { return +new Date(today.valueOf() ); }
+            // Now we would also need to update the timeout / recall checkTime() of dateSwitcherTimer
         } else {
             // console.log("Simulation: OFF");
         }
     };
 }
 
-DateSimulationController.$inject = ["moment"];
+DateSimulationController.$inject = ["moment", "$state", "$timeout"];
 
 angular
     .module('components.date')
