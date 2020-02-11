c7cafc9c30bb70702f9af08adbdd035f493869e0
Fabian Gander <gander@jumps.ch>
2020-02-11 23:32:13 +0100

    Fix projects crud

--- js/components/project/projects-crud/projects-crud.controller.js
+++ js/components/project/projects-crud/projects-crud.controller.js
@@ -1,6 +1,5 @@
 function ProjectsCrudController($state, ProjectService, cfpLoadingBar) {
     var ctrl = this;
-    var projects = ctrl.projects;
 
     // update
     ctrl.updateProject = function (event) {
@@ -8,14 +7,14 @@ function ProjectsCrudController($state, ProjectService, cfpLoadingBar) {
         return ProjectService
             // .saveProject(event.project)
             // 'projects': Helps us to save the entry directly with $save
-            .saveProject(event.project, projects)
+            .saveProject(event.project, ctrl.projects)
             .then(cfpLoadingBar.complete, cfpLoadingBar.complete);
     };
     // delete
     ctrl.deleteProject = function (event) {
         cfpLoadingBar.start();
         return ProjectService
-            .removeProject(event.project, projects)
+            .removeProject(event.project, ctrl.projects)
             .then(cfpLoadingBar.complete, cfpLoadingBar.complete);
         // .then(function() {
         //     $state.go('projects');
